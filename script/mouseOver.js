d9dde5243e081dc31eb631bc3b3aa002fa78078b
S-aint <aintlessmore@gmail.com>
2022-03-01 12:30:47 +0300

    Added different fonts

--- script/mouseOver.js
+++ script/mouseOver.js
@@ -10,7 +10,7 @@ function x(){
             }
 
             let x = randomInteger(0, 9);
-            let mass = ["arial", "serif", "times new roman", "verdana", "calibri", "arial", "serif", "times new roman", "verdana", "calibri"];
+            let mass = ["Neucha", "Caveat", "Balsamiq Sans", "Caveat", "Roboto", "Alegreya SC", "Reggae One", "Bellota", "Hachi Maru Pop", "Kaisei Decol"];
             let font = mass[x];
             spanCollection[i].style.fontFamily = font;
             }, 500);
