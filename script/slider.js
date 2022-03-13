db953b6d621418e83f4a521d9d2e2dd6fa397fdc
Svyat <aintlessmore@gmail.com>
2022-03-13 22:41:34 +0300

    Some improved slider

--- script/slider.js
+++ script/slider.js
@@ -2,7 +2,7 @@ function init(){
     for(let i = 0; i < slides.length; i++){
         let slide = slides[i];
         slide.dataset.order = i;
-        slide.style.transform = "translate(-50%, -50%)";
+        slide.style.transform = "translate(-50%, 0%)";
         slide.addEventListener("click", clickHandler);
     }
 
@@ -16,6 +16,17 @@ function update(){
     let b = height / 2 ;
     let delta = Math.PI / slides.length / 2;
 
+    let ultraleftSlide = document.querySelector(`.slide[data-order="${activeOrder - 2}"]`);
+    let anotherLeftSlide = document.querySelector(`.slide[data-order="${activeOrder - 1}"]`);
+    if(ultraleftSlide) ultraleftSlide.style.display = 'none';
+    if(anotherLeftSlide) anotherLeftSlide.style.display = 'block';
+
+    let ultraRightSlide = document.querySelector(`.slide[data-order="${activeOrder + 2}"]`);
+    let anotherRightSlide = document.querySelector(`.slide[data-order="${activeOrder + 1}"]`);
+    if(ultraRightSlide) ultraRightSlide.style.display = 'none';
+    if(anotherRightSlide) anotherRightSlide.style.display = 'block';
+
+
     for(let i = 0; i < slides.length; i++){
         let leftSlide = document.querySelector(`.slide[data-order="${activeOrder - i}"]`);
         if(leftSlide){
@@ -23,7 +34,8 @@ function update(){
             leftSlide.style.opacity = 1 - (1.3 * i) / slides.length;
             leftSlide.style.left = `${width / 2 + a * Math.cos((Math.PI * 3) / 2 - delta * i * 2)}px`;
 
-            leftSlide.style.top = `${-b * Math.sin((Math.PI * 3) / 2 - delta * i *2)}px`;
+            // leftSlide.style.top = `${-b * Math.sin((Math.PI * 3) / 2 - delta * i *2)}px`;
+            leftSlide.style.top = `calc(50vh - 200px)`;
         }
 
         let rightSlide = document.querySelector(`.slide[data-order="${activeOrder + i}"]`);
@@ -32,8 +44,11 @@ function update(){
             rightSlide.style.opacity = 1 - (1.3 * i) / slides.length;
             rightSlide.style.left = `${width / 2 + a * Math.cos((Math.PI * 3) / 2 + delta * i * 2)}px`;
 
-            rightSlide.style.top = `${-b * Math.sin((Math.PI * 3) / 2 + delta * i *2)}px`;
+            // rightSlide.style.top = `${-b * Math.sin((Math.PI * 3) / 2 + delta * i *2)}px`;
+            rightSlide.style.top = `210px`;
         }
+
+
     }
 }
 
