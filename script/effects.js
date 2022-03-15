7567427bb162ac0179965c6ea3d19fc904a6875a
Svyat <aintlessmore@gmail.com>
2022-03-16 00:14:34 +0300

    Some problems with slider

--- script/effects.js
+++ script/effects.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
+    let showSlider = new Swiper('.showcase-carousel', {
+        loop:true,
+        slidesPerView: 3,
+    });
+
+
     document.querySelector('video').playbackRate = 2;
 });
