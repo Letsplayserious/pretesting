51d0c3a8c96b340d3e91b5cb8f70783332c303f3
S-aint <aintlessmore@gmail.com>
2022-03-16 17:45:35 +0300

    Improve slider connection. Added some CSS styles.

--- script/effects.js
+++ script/effects.js
@@ -2,8 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     let showSlider = new Swiper('.showcase-carousel', {
         loop:true,
         slidesPerView: 3,
+        speed: 1800
     });
 
 
     document.querySelector('video').playbackRate = 2;
 });
+
+// new Swiper('.swiper-container');
